36e86ec202548f2e688c884b35d53189542561f0
ishan <ishan@DESKTOP-IDDG36R>
2018-10-06 23:05:03 +0530

    recreate login form and form.html form with bootstrap

--- Double_Submit_Cookie_Pattern/login.php
+++ Double_Submit_Cookie_Pattern/login.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 if ($_POST['userName'] == "ishan" && $_POST['password'] == "ishan123") {
 
     $Name = $_POST['userName'];
